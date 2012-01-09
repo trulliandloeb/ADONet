97ba5d61883f5067b369b4def73ccd2f6267b02b
trulliandloeb <trulliandloeb@hotmail.com>
2012-01-09 21:55:33 +0800

    add sample

--- ADONet/Program.cs
+++ ADONet/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +11,23 @@ namespace ADONet
     {
         static void Main(string[] args)
         {
+            var connectionString = ConfigurationManager.ConnectionStrings["localDB"].ToString();
+            Console.WriteLine(connectionString);
+            var sql = "select * from products";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using ( var command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("{0}, {1}, {2}, {3}", reader[0], reader[1], reader[2], reader[3]);
+                        }
+                    }
+                }
+            }
         }
     }
 }
